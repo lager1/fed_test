a975e54ecfcdfca4a1cc210f0746c4e8d8d2614f
lager <machv@cesnet.cz>
2017-03-19 18:27:49 +0100

    add saml login

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const passport = require('passport');
+const saml_strategy = require('passport-saml').Strategy;
+const fs = require('fs');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +31,62 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+// --------------------------------------------------------------------------------------
+
+app.get('/login',
+  passport.authenticate('saml'),
+  function(req, res, next) {
+  //  // If this function gets called, authentication was successful.
+    console.log("auth successful");
+    console.log(req.user);
+    res.redirect('/');
+  }
+);
+
+app.post('/login/callback',
+  passport.authenticate('saml'),
+  function(req, res) {
+    console.log("post login callback");
+    console.log(req.user);
+    res.redirect('/');
+  }
+);
+
+// --------------------------------------------------------------------------------------
+// saml
+
+var pvk = fs.readFileSync('cert/etlog.cesnet.cz.key.pem', 'utf-8');
+var cert = fs.readFileSync('cert/etlog.cesnet.cz.crt.pem', 'utf-8');
+var idpcert = fs.readFileSync('cert/idp-cert.pem', 'utf-8');
+//var idpcert = fs.readFileSync('cert/idp-cert2.pem', 'utf-8');
+
+var strategy = new saml_strategy({
+    callbackUrl: 'https://etlog-dev.cesnet.cz:8444/login/callback',
+    entryPoint: 'https://whoami-dev.cesnet.cz/idp/profile/SAML2/Redirect/SSO',
+    issuer: 'https://etlog-dev.cesnet.cz/',
+    protocol: 'https://',
+    decryptionPvk: pvk,
+    //ecryptionCert: cert,
+    identifierFormat : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+    cert: idpcert,
+    //privateCert: cert
+  },
+  function(profile, done) {
+    console.log("strategy");
+    console.log(profile);
+    done();
+  });
+
+passport.use(strategy);
+
+// --------------------------------------------------------------------------------------
+app.get('/metadata', function(req, res, next) {
+  res.send(strategy.generateServiceProviderMetadata(cert));
+});
+
+
+// --------------------------------------------------------------------------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
