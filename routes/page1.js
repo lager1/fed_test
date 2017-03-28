c682b40e5afca2b89c4544d89fcf0517ceda2cef
lager1 <13869941+lager1@users.noreply.github.com>
2017-03-28 23:59:04 +0200

    add debug

--- routes/page1.js
+++ routes/page1.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log("route for page1");
+  console.log(req.session);
   console.log(req.session.passport.user);
   res.send('page 1');
 });
