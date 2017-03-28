4e40163d459cd4e81a3b3bf08fde2c9dbf85d818
lager <machv@cesnet.cz>
2017-03-28 23:52:55 +0200

    add info about logged user

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fed test' });
+  var user;
+  if(req.session.passport)
+    user = "přihlášený uživatel: " + req.session.passport.user.username;
+  else
+    user = "uživatel nepříhlášen";
+
+  res.render('index', { title: 'Fed test', user : user });
 });
 
 module.exports = router;
