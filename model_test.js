3d3aae5e37184612f5aae3a5e9469615d89e0629
lager <machv@cesnet.cz>
2017-04-04 10:10:14 +0200

    add debug & return empty promise

--- model_test.js
+++ model_test.js
@@ -3,22 +3,34 @@ var exp = {};
 // --------------------------------------------------------------------------------------
 exp.getIdentityProvider = function(entityID)
 {
-
+  console.log("getIdentityProvider");
+  return new Promise(function (fullfill, reject) {
+    fullfill(null);
+  });
 }
 // --------------------------------------------------------------------------------------
 exp.storeRequestID = function(requestID, idpConfig)
 {
-
+  console.log("storeRequestID");
+  return new Promise(function (fullfill, reject) {
+    fullfill(null);
+  });
 }
 // --------------------------------------------------------------------------------------
 exp.verifyInResponseTo = function(requestID, idpConfig)
 {
-
+  console.log("verifyInResponseTo");
+  return new Promise(function (fullfill, reject) {
+    fullfill(null);
+  });
 }
 // --------------------------------------------------------------------------------------
 exp.invalidateRequestID = function(requestID, idpConfig)
 {
-
+  console.log("invalidateRequestID");
+  return new Promise(function (fullfill, reject) {
+    fullfill(null);
+  });
 }
 // --------------------------------------------------------------------------------------
 module.exports = exp;
